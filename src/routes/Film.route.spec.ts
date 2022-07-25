ec678ab602652dfb92db682691cb6ebb86a020bd
Administrator <benoit.routier@semifir.com>
2022-07-25 16:32:46 +0200

    (test): Ajout du test pour /films

--- src/routes/Film.route.spec.ts
+++ src/routes/Film.route.spec.ts
@@ -5,10 +5,22 @@ import supertest from 'supertest';
 import app from '../app';
 
 describe('Router films', () => {
-    test('GET /films', async() => {
-        // on lance la requête
-        const resp = await supertest(app).get('/films');
-        // on s'attend à un code HTTP 200
-        expect(resp.statusCode).toBe(200);
+    describe('GET /films', () => {
+        test('code 200 et reponse en JSON', async () => {
+            // on lance la requête
+            const resp = await supertest(app).get('/films');
+            // on s'attend à un code HTTP 200
+            expect(resp.statusCode).toBe(200);
+            // on s'attend à recevoir du JSON
+            expect(resp.headers['content-type']).toEqual('application/json; charset=utf-8');
+        })
+
+        test('on récupére un film', async () => {
+            const resp = await supertest(app).get('/films');
+            expect(resp.body[0]).toHaveProperty('title');
+            expect(resp.body[0]).toHaveProperty('synopsys');
+            expect(resp.body[0]).toHaveProperty('status');
+            expect(resp.body[0]).toHaveProperty('actors');
+        })
     })
 });
